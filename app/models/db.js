3cf82f9699f596806f0695f6b6b1c0c859031fa8
Alexandre Ducarre <alex@macbook-air-de-alexandre.home>
2021-05-13 13:23:50 +0200

    login update BDD

--- app/models/db.js
+++ app/models/db.js
@@ -1,5 +1,4 @@
 // const mysql = require("mysql");
-const dbConfig = require("../config/db.config.js");
 const { Pool } = require("pg");
 
 // Create a connection to the databse PostgreSQL
@@ -7,11 +6,12 @@ const pool = new Pool({
   user: "jfvrqdieasbluo",
   host: "ec2-54-195-76-73.eu-west-1.compute.amazonaws.com",
   database: "dbqc4m87m63k49",
-  password: "c728998bc4ffa16c18f5007aaf94d52b35d83beda2b59cd81a7b4afb60991cde@",
-  port: 5432
+  password: "c728998bc4ffa16c18f5007aaf94d52b35d83beda2b59cd81a7b4afb60991cde",
+  port: 5432,
+  ssl: { rejectUnauthorized: false }
 });
 console.log("Connexion réussie à la base de données");
-
+pool.connect();
 /*
 // Create a connection to the database
 const connection = mysql.createConnection({
