31f46a3a33d4958d84cdca60ab54114b21b2c226
Alexandre Ducarre <alex@MacBook-Air-de-Alexandre.local>
2021-06-09 16:21:29 +0200

    change Error Connection lost with heroku

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const bodyParser = require("body-parser");
 const fs = require("fs");
 const cors = require('cors');
 const app = express();
+const router = require('.routes/video.routes')
 app.use(cors())
+app.use(router)
 
 const port = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
 
 require("./app/routes/video.routes.js")(app);
 // set port, listen for requests
