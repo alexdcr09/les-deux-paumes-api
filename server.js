796324e39e68e75dc03ee87ec01a136ad6dd0680
Alexandre Ducarre <alex@MacBook-Air-de-Alexandre.local>
2021-06-09 16:08:50 +0200

    delete old routes in server.js

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
+const fs = require("fs");
+const cors = require('cors');
 const app = express();
-app.use(cors());
+app.use(cors())
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-});
-
-// parse requests of content-type: application/json
-app.use(bodyParser.json());
+const port = process.env.PORT || 3000;
 
 require("./app/routes/video.routes.js")(app);
 // set port, listen for requests
 app.listen(port, function () {
-    console.log("Server is running on port");
+  console.log("Server is running on port");
 });
