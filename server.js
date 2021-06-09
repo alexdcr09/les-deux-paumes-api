4d540d05f0338e9866fb99c2d72c3c7813bb54d0
Alexandre Ducarre <alex@MacBook-Air-de-Alexandre.local>
2021-06-09 16:49:08 +0200

    clean V2 server.js

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ const bodyParser = require("body-parser");
 const fs = require("fs");
 const cors = require('cors');
 const app = express();
-// const router = require('.routes/video.routes')
 app.use(cors())
-// app.use(router)
-require("./app/routes/video.routes.js")(app);
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const port = process.env.PORT || 3000;
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+require("./app/routes/video.routes.js")(app);
