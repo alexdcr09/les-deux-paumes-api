62cbdd5b7baf3b6e0f38a39d99a5823330a7ac34
Alexandre Ducarre <alex@macbook-air-de-alexandre.home>
2021-06-14 21:46:10 +0200

    fix: clean server.js + test local bdd

--- app/models/db.js
+++ app/models/db.js
@@ -3,10 +3,10 @@ const mysql = require("mysql");
 
 // Create a connection to the databse PostgreSQL
 const connection = mysql.createConnection({
-    user: "baf67f81831803",
-    host: "us-cdbr-east-04.cleardb.com",
-    database: "heroku_5aee93ca8d7911e",
-    password: "6e8d7d3c",
+    user: "MyDigitalProject",
+    host: "localhost",
+    database: "MyDigitalProject",
+    password: "66CJXWGAV7L9fQqS",
 });
 console.log("Connexion réussie à la base de données");
 connection.connect();
@@ -14,3 +14,14 @@ connection.connect();
 module.exports = connection;
 
 // mysql --host=us-cdbr-east-04.cleardb.com --user=baf67f81831803 --password=6e8d7d3c --reconnect heroku_5aee93ca8d7911e
+
+/* pw : 66CJXWGAV7L9fQqS
+const connection = mysql.createConnection({
+    user: "baf67f81831803",
+    host: "us-cdbr-east-04.cleardb.com",
+    database: "heroku_5aee93ca8d7911e",
+    password: "6e8d7d3c",
+});
+console.log("Connexion réussie à la base de données");
+connection.connect();
+*/
